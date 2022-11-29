ce44412f4b52f808caa1e60a511b89e559ec99f9
julian-b24 <julian.camilob24@gmail.com>
2022-11-29 13:06:11 -0500

    Order dto and model updated, order api definition added

---
+++ src/main/java/co/edu/icesi/JCStore/api/OrderAPI.java
@@ -0,0 +1,20 @@
+package co.edu.icesi.JCStore.api;
+
+import co.edu.icesi.JCStore.dto.OrderDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/order")
+public interface OrderAPI {
+
+    @PostMapping
+    public OrderDTO createOrder(@RequestBody OrderDTO orderDTO);
+
+    @GetMapping
+    public List<OrderDTO> getOrders();
+
+    @GetMapping("/{orderId}")
+    public OrderDTO getOrderDTO(@PathVariable UUID orderId);
+}
--- src/main/java/co/edu/icesi/JCStore/model/Order.java
+++ src/main/java/co/edu/icesi/JCStore/model/Order.java
@@ -1,16 +1,25 @@
 package co.edu.icesi.JCStore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
 import java.util.UUID;
 
 @Data
-@Table
-public class TheOrder {
+@Table(name = "`orders`")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
 
     @Id
+    @Type(type = "org.hibernate.type.PostgresUUIDType")
     private UUID orderId;
 
     @Column(name = "total")
@@ -28,4 +37,12 @@ public class TheOrder {
             joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> orderItems;*/
+
+    @ManyToOne(cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE
+    }, fetch = FetchType.EAGER, targetEntity = User.class)
+    @JoinColumn(name = "id")
+    @Type(type = "org.hibernate.type.PostgresUUIDType")
+    private UUID userId;
+
 }
