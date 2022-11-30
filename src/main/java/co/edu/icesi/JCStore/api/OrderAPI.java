8e6a7f6c2767fdebac8be24ca845832ed01a2b75
julian-b24 <julian.camilob24@gmail.com>
2022-11-30 09:38:40 -0500

    Request mapping route for orders changed

--- src/main/java/co/edu/icesi/JCStore/api/OrderAPI.java
+++ src/main/java/co/edu/icesi/JCStore/api/OrderAPI.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public interface OrderAPI {
 
     @PostMapping
